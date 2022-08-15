88645e9cd1abcd02be5be60b6a755b0a38b35a1f
josebayo <dgjmpb@gmail.com>
2022-08-15 19:30:03 +0200

    new file variables

---
+++ JS/cursoJavaScriptPrincipiante/iniciando.js
@@ -0,0 +1,120 @@
+// first global variable
+const WHATEVER = 'Whatever';
+console.log(WHATEVER);
+// second global variable
+const COUNTRY_LOCATION = 'United Sates Florida';
+console.log(COUNTRY_LOCATION);
+// third global variable
+const DOG_CAT = 'Miguel Angel';
+console.log(DOG_CAT);
+// fourth global variable
+const CAR_MODEL = 'Ford Fiesta';
+console.log(CAR_MODEL);
+// fifth  global variable
+const EAT = 'Banana';
+console.log(EAT);
+// sixth global variable
+const PEN = 'Pilot';
+console.log(PEN);
+// seventh global variable
+const APPLIANCE = 'Microwave';
+console.log(APPLIANCE);
+// eighth global variable
+const FURNITURE = 'Table';
+console.log(FURNITURE);
+// nineth global variable
+const BODY = 'Girl';
+console.log(BODY);
+// tenth global variable
+const TOY = 'Satisfallet';
+console.log(TOY);
+// eleventh global variable
+const N1 = 1;
+console.log(N1);
+// twelfth global variable
+const N2 = 2;
+console.log(N2);
+// thirteenth global variable
+const N3 = 3;
+console.log(N3);
+// fourteenth global variable
+const N4 = 4;
+console.log(N4);
+// fiveteenth global variable
+const N5 = 5;
+console.log(N5);
+// sixteenth global variable
+const N6 = 6;
+console.log(N6);
+// seventh global variable
+const N7 = 7;
+console.log(N7);
+// eighteenth global variable
+const N8 = 8;
+console.log(N8);
+// nineteenth global variable
+const N9 = 9;
+console.log(N9);
+// twenty global variable
+const N10 = 10;
+console.log(N10);
+// twenty-one global variable
+const D1 = 34.45;
+console.log(D1);
+// twenty-two global variable
+const D2 = 32.25;
+console.log(D2);
+// twenty-third global variable
+const D3 = 36.35;
+console.log(D3);
+// twenty-four global variable
+const D4 = 35.7;
+console.log(D4);
+// twenty-fifth global variable
+const D5 = 33.47;
+console.log(D5);
+// twenty-sixth global variable
+const D6 = 34.2;
+console.log(D6);
+// twenty-seventh global variable
+const D7 = 43.78;
+console.log(D7);
+// twenty-eighth global variable
+const D8 = 25.23;
+console.log(D8);
+// twenty-ninth global variable
+const D9 = 923.236;
+console.log(D9);
+// thirty global variable
+const D10 = 23.42;
+console.log(D10);
+// thirty-first global variable
+const ON1 = new Number (23);
+console.log(ON1);
+// thirty-two global variable
+const ON2 = new Number (24);
+console.log(ON2);
+// thirty-third global variable
+const ON3 = new Number (25);
+console.log(ON3);
+// thirty-fourth global variable
+const ON4 = new Number (324);
+console.log(ON4);
+// thirty-fifth global variable
+const ON5 = new Number (125);
+console.log(ON5);
+// thirty-sixth global variable
+const ON6 = new Number (235);
+console.log(ON6);
+// thirty-seventh global variable
+const ON7 = new Number (255);
+console.log(ON7);
+// thirty-eighth global variable
+const ON8 = new Number (325);
+console.log(ON8);
+// thirty-ninth global variable
+const ON9 = new Number (295);
+console.log(ON9);
+// fortieth global variable
+const ON10 = new Number (3225);
+console.log(ON10);
