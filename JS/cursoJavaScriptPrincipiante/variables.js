afbabbda449af522b425a0369c45966ae5a79b15
josebayo <dgjmpb@gmail.com>
2022-08-16 13:14:15 +0200

    new change 13:14

---
+++ JS/cursoJavaScriptPrincipiante/variables.js
@@ -0,0 +1,20 @@
+// global variables
+const HOUSE = 'Word';
+console.log(HOUSE)
+// local variables
+// const
+// cannot change its value
+const NUMBER = 1;
+console.log(NUMBER)
+const NAME = 'Rogar'
+console.log(NAME)
+// local variable
+// let
+// can change its value, limited
+let TEXT = 'Hello Mike'
+console.log(TEXT)
+// local variable
+// var
+// can change its value, not limited
+var DENOMINATION = 'Programming Brothers'
+console.log(DENOMINATION)
