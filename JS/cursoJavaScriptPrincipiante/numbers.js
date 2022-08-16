ad6b44410817bb677c27ad3fd65764755abf9fde
josebayo <dgjmpb@gmail.com>
2022-08-16 17:34:40 +0200

    first numbers.js

---
+++ JS/cursoJavaScriptPrincipiante/numbers.js
@@ -0,0 +1,8 @@
+/*  Exercise 1:
+    The price of the vehicle es 25000€ in cash. 
+    Financed the price would be 24000€ (3000€ + 28% interest).
+    Registration fee 4000€, traffic tax 3000€, town hall stamp 1000€.
+    Cash dicount 2%, financed discount 3%.
+    Get the total price in both cases.
+*/
+const 
