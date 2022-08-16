814b96f4dd3380cbd3e0efae9a8abc431a4f6fd7
josebayo <dgjmpb@gmail.com>
2022-08-16 18:38:57 +0200

    finished numbers.js

--- JS/cursoJavaScriptPrincipiante/numbers.js
+++ JS/cursoJavaScriptPrincipiante/numbers.js
@@ -5,4 +5,26 @@
     Cash dicount 2%, financed discount 3%.
     Get the total price in both cases.
 */
-const 
+var PRICE_CASH_VEHICLE = 25000 + 4000 + 3000 + 1000;
+console.log(PRICE_CASH_VEHICLE);
+var PRICE_FINANCED_VEHICLE = 24000;
+console.log(PRICE_FINANCED_VEHICLE);
+var CASH_DISCOUNT_VEHICLE = (PRICE_CASH_VEHICLE * 2 / 100);
+console.log(PRICE_CASH_VEHICLE - CASH_DISCOUNT_VEHICLE);
+var FINANCED_DISCOUNT_VEHICLE = (PRICE_FINANCED_VEHICLE * 3 / 100);
+console.log(PRICE_FINANCED_VEHICLE - FINANCED_DISCOUNT_VEHICLE);
+var INTEREST = 24000 * 24 / 100;
+console.log(INTEREST);
+//Exercise 1 version 2
+var priceCashVehicle = 25000 + 4000 + 3000 + 1000;
+console.log(priceCashVehicle);
+var priceFinancedVehicle = 24000;
+console.log(priceFinancedVehicle);
+var cashDiscountVehicle = (priceCashVehicle * 2 / 100);
+console.log(priceCashVehiclee - cashDiscountVehicle);
+var financedDiscountVehicle = (priceFinancedVehicle * 3 / 100);
+console.log(priceFinancedVehicle - financedDiscountVehicle);
+var interest = 24000 * 24 / 100;
+console.log(interest);
+var totalInterest = (interest + 3000)
+console.log(totalInterest)
