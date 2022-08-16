12aae3ce13d5e185ac004fcba8f3477d33ce5f78
josebayo <dgjmpb@gmail.com>
2022-08-16 13:27:32 +0200

    new file, variables, completed

--- JS/cursoJavaScriptPrincipiante/variables.js
+++ JS/cursoJavaScriptPrincipiante/variables.js
@@ -1,6 +1,8 @@
 // global variables
 const HOUSE = 'Word';
 console.log(HOUSE)
+const PROVINCE_CITY = 'Andalucía, Granada'
+console.log(PROVINCE_CITY)
 // local variables
 // const
 // cannot change its value
@@ -10,11 +12,11 @@ const NAME = 'Rogar'
 console.log(NAME)
 // local variable
 // let
-// can change its value, limited
-let TEXT = 'Hello Mike'
-console.log(TEXT)
+// can change its value, limited, you have to define the variables
+let compoundText = 'Hello Mike'
+console.log(compoundText)
 // local variable
 // var
-// can change its value, not limited
-var DENOMINATION = 'Programming Brothers'
-console.log(DENOMINATION)
+// can change its value, not limited, you don't have to define the variable at the moment
+var denomination = 'Programming Brothers'
+console.log(denomination)
