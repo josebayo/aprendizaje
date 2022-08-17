72b9ea3f75b548af2af86d78b05a4217196b848a
josebayo <dgjmpb@gmail.com>
2022-08-17 13:47:04 +0200

    update 13:46

--- JS/cursoJavaScriptPrincipiante/javascriptGettingStartedExercises.js
+++ JS/cursoJavaScriptPrincipiante/javascriptGettingStartedExercises.js
@@ -1,1 +1,22 @@
-console.log("sflkf");
+//ASK GOR THE AGE
+var age = prompt("age");
+//CHECK THAT WHAT YOU ENTERED IS A NUMBER
+if (Number(age) == age) {
+    //IF YOU ARE OVER 18
+    if (age => 18) {
+        //PRINTS THE MESSAGE ON THE SCREEN
+        document.write("Ok, your can driver")
+    }
+}
+if (Number (age) == age) {
+    //IF YOU ARE UNDER 18
+    if (age < 18) {
+        //PRINTS THE MESSAGE ON THE SCREEN
+        document.write("Mistake, your can't driver")
+    }
+} 
+//OTHERWISE
+else {
+    //SHOW A MESSAGE
+    alert("Invalid number")
+}
