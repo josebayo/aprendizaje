c07049eff5543a9bba4829009550cb09d7b548e6
josebayo <dgjmpb@gmail.com>
2022-08-17 18:09:04 +0200

    update 18:09

--- JS/cursoJavaScriptPrincipiante/javascriptGettingStartedExercises.js
+++ JS/cursoJavaScriptPrincipiante/javascriptGettingStartedExercises.js
@@ -3,16 +3,13 @@ var age = prompt("age");
 //CHECK THAT WHAT YOU ENTERED IS A NUMBER
 if (Number(age) == age) {
     //IF YOU ARE OVER 18
-    if (age => 18) {
+    if (age >= 18) {
         //PRINTS THE MESSAGE ON THE SCREEN
-        document.write("Ok, your can driver")
+        document.write("Ok, you can drive")
     }
-}
-if (Number (age) == age) {
-    //IF YOU ARE UNDER 18
-    if (age < 18) {
+    else if (age < 18) {
         //PRINTS THE MESSAGE ON THE SCREEN
-        document.write("Mistake, your can't driver")
+        document.write("Mistake, you can't drive")
     }
 } 
 //OTHERWISE
