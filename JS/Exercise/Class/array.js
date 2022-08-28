21df9fd67eea73a6bed564181303796730256431
josebayo <dgjmpb@gmail.com>
2022-08-28 12:39:39 +0200

    new file array.js

---
+++ JS/Exercise/Class/array.js
@@ -0,0 +1,13 @@
+// Exercise 1
+var cars = ["citroën", "renault", "ford"];
+
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+console.log(cars[3]);
+
+cars.push("opel");
+
+console.log(cars);
+
+console.log(cars[3]);
