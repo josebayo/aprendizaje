fc0ca4c269a1704bc698b42f5f727c9b0185760f
josebayo <dgjmpb@gmail.com>
2022-09-01 11:30:47 +0200

    New_folder_Event

--- JS/Exercise/Event/Event.js
+++ JS/Exercise/Event/Event.js
@@ -1,3 +1,49 @@
 /*function clic() {
     alert(":3");
 }*/
+
+/*
+document.getElementById("miBtn"). addEventListener("click",click,true);
+
+function clic() {
+    alert("3");
+}*/
+
+
+/*document.getElementById("myDiv").addEventListener("click",anotherClic,true);
+
+document.getElementById("miBtn"). addEventListener("click",click,false);
+
+function clic(){
+    alert("Esto fue disparado por el botón");
+}
+
+function anotherClic() {
+    alert("Esto fue disparado por div");
+}*/
+
+
+/*document.getElementById("myDiv").addEventListener("click",anotherClic,false);
+
+document.getElementById("miBtn"). addEventListener("click",click,true);
+
+function clic(){
+    alert("Esto fue disparado por el botón");
+}
+
+function anotherClic() {
+    alert("Esto fue disparado por div");
+}*/
+
+
+document.getElementById("myDiv").addEventListener("click",anotherClic,false);
+
+document.getElementById("miBtn"). addEventListener("click",click,false);
+
+function clic(){
+    alert("Esto fue disparado por el botón");
+}
+
+function anotherClic() {
+    alert("Esto fue disparado por div");
+}
