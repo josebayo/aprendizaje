5d4748f742e08d286bc3e6194a0b37a1521005ed
josebayo <dgjmpb@gmail.com>
2022-09-06 10:07:51 +0200

    New file: funtion.js

---
+++ JS/Exercise/function/funtion.js
@@ -0,0 +1,20 @@
+function myCars(brand, model, colour) {
+    console.log("Brand: " + brand + ", Model: " + model + ", Colour: " + colour);
+}
+
+var brandCar = "Citroën";
+var modelCar = "C4";
+var colourCar = "White";
+
+myCars(brandCar, modelCar, colourCar);
+
+
+/*
+function add(a,b,c) {
+    return a+b+c;
+}
+
+var result = add(1,2,3);
+
+console.log(result)
+*/
