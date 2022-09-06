9686923e0b807612587a9c49ca1feb1dcbc891cf
josebayo <dgjmpb@gmail.com>
2022-09-06 17:36:14 +0200

    New file arrow.js

--- JS/Exercise/arrow/arrow.js
+++ JS/Exercise/arrow/arrow.js
@@ -1,21 +1,43 @@
 // Arrow number
+/*
 var number = [3, 6, 4, 8];
 
-var number2 = number.map((b) => { ; return b * b; });
+var number2 = number.map((b) => return b * b);
 
 console.log(number2);
+*/
 
 
 //Arrow string
-function name {
+/*
+function Name() {
     this.realName = "Josep";
 
     setTimeout(function () {
         this.realName = "Pesoj";
 
-    }, 200)
+    }, 200);
 }
 
-var Name = new name();
+
+var name = new Name();
 
 console.log(name);
+
+console.log(name.realName);
+*/
+
+
+// Change function nomal to function arrow
+var nameArrow = () => {
+    var realName = "Josep";
+
+    console.log(realName);
+
+    setTimeout(function () {
+        realName = "Pesoj";
+        console.log(realName);
+    }, 200);
+};
+
+nameArrow();
