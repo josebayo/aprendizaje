d0b35e4705080ab608745cc0b0fdd1d9a0ae688a
josebayo <dgjmpb@gmail.com>
2022-09-11 19:01:03 +0200

    New exercise, SingleAndDoubleQuote

---
+++ JS/Exercise/Others/SingleAndDoubleQuotes/SingleAndDoubleQuotes.js
@@ -0,0 +1,10 @@
+//Single and double quotes
+var mesenger = "Hello world! \n How easy it is to include \'single quote\' \n and \"double quotes.\""
+
+//Arrow function
+var showText = (text) => {
+    alert(text);
+    //console.log(text);
+};
+
+showText(mesenger);
